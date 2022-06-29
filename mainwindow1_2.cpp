f1a1d651557571df05e00cf05b3c5dd114f57d66
TrueVegetable <2100012960@stu.pku.edu.cn>
2022-06-29 10:35:23 +0800

    try to debug

--- mainwindow1_2.cpp
+++ mainwindow1_2.cpp
@@ -86,7 +86,7 @@ void MainWindow1_2::on_CS2btm_clicked()
     }
 }
 
-void MainWindow1_2::read_archive(char const* filename){
+/*void MainWindow1_2::read_archive(char const* filename){
 
     std::ifstream  inFile(filename,std::ios::in);
     MainWindow2 * gw2=new MainWindow2;
@@ -118,8 +118,59 @@ void MainWindow1_2::read_archive(char const* filename){
     inFile>>gw2->my;
     inFile.close();
     gw2->show();
-}
+}*/
+void MainWindow1_2::read_archive(char const* filename){
 
+    std::ifstream  inFile(filename,std::ios::in);
+    int level; inFile>>level;
+    MainWindow2 * gw2=new MainWindow2(filename,level);
+    std::cout<<"here1"<<std::endl;
+    int id,px,py,mhp,hp,matk,cd1,cd2,cd3;
+    inFile>>id>>px>>py>>mhp>>hp>>matk;
+    inFile>>cd1>>cd2>>cd3;
+    gw2->self = new CCreature(id,px,py,mhp,matk,gw2->use_ui()->centralwidget,":/new/prefix1/me.png");
+    gw2->self->hp = hp;
+    gw2->self->atk.push_back(Attack(0,3+(level+1)/3,std::min(5,3+(level+2)/5),"Shoot",1));
+    gw2->self->atk.push_back(Attack(1,2+(level+2)/3,std::min(6,4+level/5),"Bomb",3));
+    gw2->self->atk.push_back(Attack(2,3+level/3,std::min(7,5+level/4),"Arrow",2)); //赋予武器
+    gw2->self->atk[0].curcd=cd1;
+    gw2->self->atk[1].curcd=cd2;
+    gw2->self->atk[2].curcd=cd3;
+    gw2->self->upd();
+    std::cout<<"here2"<<std::endl;
+    plax = &(gw2->self->posx); play= &(gw2->self->posy);
+    gw2->clist.clear();
+    for(int i=0;i<TSizeX;i++){
+        for(int j=0;j<TSizeY;j++)
+            if(gw2->cmp[i][j]) gw2->cmp[i][j] = nullptr;
+    }
+    std::cout<<"here3"<<std::endl;
+    gw2->clist.push_back(gw2->self);
+    gw2->cmp[px][py] = gw2->self;
+    std::cout<<"here3.5"<<std::endl;
+    int len_clist;
+    inFile>>len_clist;
+    for(int i =1;i<len_clist;i++){
+        inFile>>id>>px>>py>>mhp>>hp>>matk;
+        std::cout<<"here41"<<std::endl;
+        std::cout<<id<<" "<<px<<" "<<py<<" "<<mhp<<" "<<matk<<std::endl;
+        CCreature* c1 = new CCreature(id,px,py,mhp,matk,gw2->use_ui()->centralwidget,":/new/prefix1/Enemy.png");
+        std::cout<<"here42"<<std::endl;   //这里开始就不会输出了？
+        c1->hp = hp;
+        std::cout<<"here43"<<std::endl;
+        c1->upd();
+        std::cout<<"here44"<<std::endl;
+        gw2->cmp[px][py] = c1;
+        std::cout<<"here46"<<std::endl;
+        gw2->clist.push_back(c1);
+    }
+    std::cout<<"here45"<<std::endl;
+    inFile>>gw2->mode;
+    inFile>>gw2->mx;
+    inFile>>gw2->my;
+    inFile.close();
+    gw2->show();
+}
 /* 存储格式：.txt
  * row1: self ( in sequence: int Id,int px,int py,int mhp,int hp,int mATK )
  * row2: size of clist - n
