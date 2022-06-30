c8b67829614c8f05a2e8208e682f025ceb056b7c
TrueVegetable <2100012960@stu.pku.edu.cn>
2022-06-30 17:27:49 +0800

    0630 by czs: add enemies

--- CCreature.h
+++ CCreature.h
@@ -27,28 +27,19 @@ public:
     QLabel * pic; //显示图片
     QProgressBar * hpbar; //血条
     vector<Attack> atk; //当前可用的攻击列表
-    CCreature(int id,int px,int py,int HP,int mATK,QWidget *parent,string Path);
+    CCreature(int id,int px,int py,int HP,int mATK,QWidget *parent,string Path,int SADD);
     void Move(int nx,int ny); //移动到(nx,ny)
     void rAtk(CCreature * & tar,Attack & A); //该生物使用攻击A远程攻击生物tar
     void upd(); //更新状态（目前只有血条）
     bool Attack(CCreature * target); //近身攻击target
     int FindPath(CCreature * mp[XMX][YMX],int map[XMX][YMX],int x = *plax,int y = *play); //用bfs找到当前到(x,y)的路线，返回当前该走的方向
     ~CCreature();
+    int SAD = 0; //标记敌人种类，S-近身攻击的Soldier 0;A-Archer，可远程攻击rAtk，血量更低，但攻击力更高 1;D-Demoman，可使用bomb，使用前一轮会产生提示 2
+    //缺省 = 0
+    void speci(); //通过SAD确认身份，并更改不同种类士兵属性
+    bool Range_Ar(int tax,int tay); //判断me是否在Archer的攻击范围内
+    bool Range_De(int taxx,int tayy); //判断me是否在Demoman的攻击范围内
+    int use_time = 3; //武器使用次数，固定为3
 };
-
-class Archer:public CCreature{
-public:
-    Archer(int id,int px,int py,int HP,int mATK,QWidget *parent,string Path):CCreature(id,px,py,HP,mATK,parent,Path)
-    {
-
-    }
-}; //弓箭手，可远程攻击rAtk，血量更低，但攻击力更高
-class Demoman:public CCreature{
-public:
-    Demoman(int id,int px,int py,int HP,int mATK,QWidget *parent,string Path):CCreature(id,px,py,HP,mATK,parent,Path)
-    {
-
-    }
-}; //爆破兵，可使用bomb，使用前一轮会产生提示
 
 #endif // CCREATURE_H
